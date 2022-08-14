494c0e560f9b44b682ce8efc4867539123fc86c8
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-08-14 23:11:50 +0400

    Improved documentation and security

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,15 +1,25 @@
 import { soldTicketsStorage, Ticket, ticketsStorage } from './model';
 import { context, ContractPromiseBatch } from 'near-sdk-as';
 
+
+/**
+ * @dev allows users to create a new ticket Event
+ * @param newTicket object containing values to create a new instance of Ticket
+ */
 export function initialTicket(newTicket: Ticket): void {
     let ticket = ticketsStorage.get(newTicket.id);
     if (ticket) {
         throw new Error(`A ticket with id=${ticket.id} already exists`);
     }
-
+    assert(newTicket.remaining > u32.MIN_VALUE, "At least one ticket must be available");
     ticketsStorage.set(newTicket.id, Ticket.initial(newTicket));
 }
 
+/**
+ * @dev allows a ticket event's owner to increase the number of tickets remaining
+ * @param ticketId id of Ticket
+ * @param quantity increasing amount for number of tickets
+ */
 export function increaseTickets(ticketId: string, quantity: u32): void {
     let ticket = ticketsStorage.get(ticketId);
     if (!ticket) {
@@ -18,11 +28,16 @@ export function increaseTickets(ticketId: string, quantity: u32): void {
     if (ticket.owner != context.sender) {
         throw new Error("You don't have permission");
     }
-
-    ticket.remaining += quantity;
+    assert(quantity > u32.MIN_VALUE, "Quantity must be at least one");
+    ticket.increaseTickets(quantity);
     ticketsStorage.set(ticketId, ticket);
 }
 
+/**
+ * @dev allows a ticket event's owner to decrease the number of tickets remaining
+ * @param ticketId id of Ticket
+ * @param quantity decreasing amount for number of tickets
+ */
 export function reduceTickets(ticketId: string, quantity: u32): void {
     let ticket = ticketsStorage.get(ticketId);
     if (!ticket) {
@@ -31,15 +46,15 @@ export function reduceTickets(ticketId: string, quantity: u32): void {
     if (ticket.owner != context.sender) {
         throw new Error("You don't have permission");
     }
-
-    if (ticket.remaining < quantity) {
-        ticket.remaining = 0;
-    } else {
-        ticket.remaining -= quantity;
-    }
+    assert(quantity > u32.MIN_VALUE, "Quantity must be at least one");
+    ticket.decreaseTickets(quantity);
     ticketsStorage.set(ticketId, ticket);
 }
 
+/**
+ * @dev allows users to buy tickets from a ticket event
+ * @param ticketId id of Ticket
+ */
 export function buyTicket(ticketId: string): void {
     const ticket = getTicket(ticketId);
     if (!ticket) {
@@ -49,11 +64,8 @@ export function buyTicket(ticketId: string): void {
     if (!ticket.checkTicketAvailable()) {
         throw new Error('Tickets sold out');
     }
-
-    if (ticket.price.toString() > context.attachedDeposit.toString()) {
-        throw new Error("Attached deposit should be greater than the ticket's price");
-    }
-
+    assert(context.sender.toString() != ticket.owner.toString(), "You can't buy your own tickets");
+    assert(ticket.price.toString() == context.attachedDeposit.toString(), "Attached deposit must be equal to the ticket's price");
     ContractPromiseBatch.create(ticket.owner).transfer(context.attachedDeposit);
     ticket.soldTicket();
     ticketsStorage.set(ticket.id, ticket);
